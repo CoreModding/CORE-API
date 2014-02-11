a0ab26d7a1a22fadd9d669aa733a726502c49b40
ProofTheEngineer <prooftheengineer@gmail.com>
2014-02-11 16:15:04 +0100

    Some random code that is usefull

--- info/coremodding/api/CoreAPI.java
+++ info/coremodding/api/CoreAPI.java
@@ -7,6 +7,16 @@ import cpw.mods.fml.common.Mod;
  */
 @Mod(modid = "CoreModding_LIB", name = "CoreModding API", version = "0.0.1")
 public class CoreAPI {
-
+	
+    // Don't remove these strings they are getting used later!
+	
+	//ModID of this mod.
+	public static final String ModID = "CoreModding_LIB";
+	
+	//The Mods name.
+	public static final String ModName = "CoreModding API";
+	
+	//The Version of this mod.
+	public static final String ModVer = "0.0.1";
 	
 }
