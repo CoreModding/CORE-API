fd0b26ac30eeb32f14f37d9083a5559a7133c0f1
ProofTheEngineer <ProofTheEngineer@users.noreply.github.com>
2014-03-01 15:21:08 +0100

    Added some GOOD utils for LogHelper.java
    Don't even do "Removed pointless stuff"

--- info/coremodding/api/output/LogHelper.java
+++ info/coremodding/api/output/LogHelper.java
@@ -1,17 +1,37 @@
 package info.coremodding.api.output;
 
+import java.util.Date;
+import java.util.logging.Level;
+
 /**
  * Logging utils
  */
 public class LogHelper {
 
 	/**
-	 * Basic logging functionality
+	 * logging functionality
 	 * 
-	 * @param msg
-	 *            The message
+	 * @param msg The message
+	 * @param lvl The level of the log
 	 */
-	public static void log(String msg) {
-		System.out.println(msg);
+	public static void log(String msg, Level lvl){
+		Date date = new Date();
+		String time = date.toString();
+		System.out.println("[" + time + "]" + " [" + lvl + "] "+" :" + msg);
+	}
+	
+	/**
+	 * logging functionality
+	 * 
+	 * @param msg The message
+	 * @param dbl The Debug Mode Booleans Location
+	 * @param lvl The level of the log
+	 */
+	public static void log(String msg, Level lvl ,boolean bdl){
+		if (bdl == true){
+			Date date = new Date();
+			String time = date.toString();
+			System.out.println("[" + time + "]" + " [" + lvl + "] "+" :" + msg);
+		}
 	}
 }
