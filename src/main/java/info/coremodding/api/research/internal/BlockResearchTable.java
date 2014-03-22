94aafbbfe8d61a194881abbfe4f7cb5bb1b688e6
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-22 12:46:28 -0700

    A start, attempting to figure out GUI id.

---
+++ src/main/java/info/coremodding/api/research/internal/BlockResearchTable.java
@@ -0,0 +1,46 @@
+package info.coremodding.api.research.internal;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+/**
+ * @author James
+ *         The research table block class
+ */
+public class BlockResearchTable extends BlockContainer
+{
+    
+    /**
+     * The constructor
+     */
+    public BlockResearchTable()
+    {
+        super(Material.wood);
+        this.setCreativeTab(CreativeTabs.tabDecorations);
+        this.setBlockName("Research Table");
+        this.setHardness(5.0F);
+        this.setLightLevel(10);
+        this.setResistance(3.0F);
+    }
+    
+    @Override
+    public TileEntity createNewTileEntity(World var1, int var2)
+    {
+        return new TileEntityResearchTable();
+    }
+    
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z,
+            EntityPlayer player, int metadata, float what, float these,
+            float are)
+    {
+        TileEntity tileEntity = world.getTileEntity(x, y, z);
+        if (tileEntity == null || player.isSneaking()) { return false; }
+        // TODO player.openGui(CoreAPI.instance, modGuiId, world, x, y, z);
+        return true;
+    }
+}
