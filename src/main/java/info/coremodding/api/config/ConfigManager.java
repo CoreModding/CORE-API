50631bcc3026e176b0a4ba2ea08944ccb1ac48ab
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-30 20:30:30 -0700

    Fixes

---
+++ src/main/java/info/coremodding/api/config/ConfigManager.java
@@ -0,0 +1,54 @@
+package info.coremodding.api.config;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Some forge config utilities
+ */
+public class ConfigManager
+{
+    
+    /**
+     * The configuration instance
+     */
+    public static Configuration config = new Configuration();
+    
+    /**
+     * Creates the config file itself
+     * 
+     * @param event
+     *            The event used to get the directory
+     * @param dir
+     *            The sub-directory
+     * @param modidName
+     *            The mod id to name the config
+     */
+    public static void CreateConfig(FMLPreInitializationEvent event,
+            String dir, String modidName)
+    {
+        config = new Configuration(new File(event
+                .getModConfigurationDirectory().getAbsolutePath()
+                + File.separator + dir + File.separator + modidName + ".cfg"));
+    }
+    
+    /**
+     * Gets a value
+     */
+    public static void get(String category, String key, String defaultValue,
+            String comment)
+    {
+        config.get(category, key, defaultValue, comment);
+    }
+    
+    /**
+     * Gets a value
+     */
+    public static void get(String category, String key, boolean[] defaultValue,
+            String comment)
+    {
+        config.get(category, key, defaultValue, comment);
+    }
+}
