1951679c16b3d83afc5462467a556b1381fef27b
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-31 22:17:48 -0700

    Started off, havent tested yet.

---
+++ src/main/java/info/coremodding/api/internal/downloader/GPSDownloader.java
@@ -0,0 +1,38 @@
+package info.coremodding.api.internal.downloader;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author James
+ *         Automatically downloads or updates GPS when core-api is installed so
+ *         people only need to download one thing
+ */
+public class GPSDownloader
+{
+    
+    /**
+     * Does the update
+     */
+    public static void doUpdate()
+    {
+        try
+        {
+            for (File file : new File(".\\").listFiles())
+            {
+                if (file.getName().startsWith("GPS")) file.delete();
+            }
+            FileUtils
+                    .copyURLToFile(
+                            new URL(
+                                    "https://github.com/CoreModding/GodlyEnergySystems/raw/master/dls/APIDist1_7.jar"),
+                            new File(".\\GPS_Automatic.jar"));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
