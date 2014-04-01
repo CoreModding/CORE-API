77b1dd9750dad8f026c3d69bf29917a20bcdcc27
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-31 17:44:12 -0700

    Added all converters

--- src/main/java/info/coremodding/api/locations/EDirection.java
+++ src/main/java/info/coremodding/api/locations/EDirection.java
@@ -1,5 +1,9 @@
 package info.coremodding.api.locations;
 
+import info.coremodding.api.locations.exceptions.InvalidDirectionException;
+
+import java.util.ArrayList;
+
 /**
  * @author James The sides of things
  */
@@ -7,37 +11,129 @@ public enum EDirection
 {
     
     /**
-     * An unset direction or all sides
+     * All sides
      */
-    all,
+    all("All"),
+    
+    /**
+     * No side
+     */
+    none("None", new String[] { "Null", "NA", "N/A", "Invalid" }),
     
     /**
      * The direction is below
      */
-    bottom,
+    bottom("Bottom"),
     
     /**
      * The direction is east
      */
-    east,
+    east("East"),
     
     /**
      * The direction is north
      */
-    north,
+    north("North"),
     
     /**
      * The direction is south
      */
-    south,
+    south("South"),
     
     /**
      * The direction is above
      */
-    top,
+    top("Top"),
     
     /**
      * The direction is west
      */
-    west
+    west("West");
+    
+    private String                       StringValue;
+    private final ArrayList<String>      alts        = new ArrayList<>();
+    private static ArrayList<EDirection> edirections = new ArrayList<>();
+    
+    private EDirection(String name)
+    {
+        this.StringValue = name;
+        addEDirection();
+    }
+    
+    private EDirection(String name, String[] alts)
+    {
+        this.StringValue = name;
+        for (String alt : alts)
+        {
+            this.alts.add(alt);
+        }
+        addEDirection();
+    }
+    
+    private void addEDirection()
+    {
+        EDirection.edirections.add(this);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return this.StringValue;
+    }
+    
+    /**
+     * @return The alternate names of this
+     */
+    public ArrayList<String> getAlts()
+    {
+        return this.alts;
+    }
+    
+    /**
+     * @param name
+     *            The name of the EDirction. Valid:
+     *            all
+     *            bottom
+     *            east
+     *            invalid
+     *            none
+     *            north
+     *            null
+     *            na
+     *            n/a
+     *            south
+     *            top
+     *            west
+     * @return The EDirection with that name.
+     * @throws InvalidDirectionException
+     *             The given name is invalid
+     */
+    public static EDirection getFromName(String name)
+            throws InvalidDirectionException
+    {
+        for (EDirection edirection : edirections)
+        {
+            if (edirection.toString().equalsIgnoreCase(name)) return edirection;
+            ArrayList<String> alts = edirection.getAlts();
+            for (String alt : alts)
+            {
+                if (alt.equalsIgnoreCase(name)) return edirection;
+            }
+        }
+        throw new InvalidDirectionException();
+    }
+    
+    /**
+     * @param direction
+     *            The compass direction
+     * @return The EDirection equivalent to that compassdirection
+     */
+    public static EDirection getFromCompass(ECompassDirection direction)
+    {
+        if (direction == ECompassDirection.north) { return EDirection.north; }
+        if (direction == ECompassDirection.east) { return EDirection.east; }
+        if (direction == ECompassDirection.south) { return EDirection.south; }
+        if (direction == ECompassDirection.west) { return EDirection.west; }
+        return EDirection.none;
+    }
 }
