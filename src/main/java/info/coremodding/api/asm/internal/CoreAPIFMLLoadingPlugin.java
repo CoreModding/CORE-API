6af8c44d2fb84db21849e8d9fef95166e83cc7b0
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-04-05 11:08:45 -0700

    Created loading plugin

---
+++ src/main/java/info/coremodding/api/asm/internal/CoreAPIFMLLoadingPlugin.java
@@ -0,0 +1,44 @@
+package info.coremodding.api.asm.internal;
+
+import java.util.Map;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+/**
+ * @author James
+ *         The FML loading plugin for Core-API
+ */
+public class CoreAPIFMLLoadingPlugin implements IFMLLoadingPlugin
+{
+    
+    @Override
+    public String[] getASMTransformerClass()
+    {
+        return new String[] { CoreAPIClassTransformer.class.getName() };
+    }
+    
+    @Override
+    public String getModContainerClass()
+    {
+        return CoreAPIAsm.class.getName();
+    }
+    
+    @Override
+    public String getSetupClass()
+    {
+        return null;
+    }
+    
+    @Override
+    public void injectData(Map<String, Object> data)
+    {
+        return;
+    }
+    
+    @Override
+    public String getAccessTransformerClass()
+    {
+        return null;
+    }
+    
+}
