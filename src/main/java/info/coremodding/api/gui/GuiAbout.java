2fcf4ec957a9ce07964cda16a1631d9379f4e093
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-04-07 16:30:02 -0700

    Tiny code cleans

--- src/main/java/info/coremodding/api/gui/GuiAbout.java
+++ src/main/java/info/coremodding/api/gui/GuiAbout.java
@@ -1,95 +1,103 @@
 package info.coremodding.api.gui;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author cpw
- *
+ * 
  */
 public class GuiAbout extends GuiScreen
 {
-    private GuiScreen mainMenu;
-    private int listWidth;
+    
+    private final GuiScreen mainMenu;
+    private final int       listWidth;
+    
     /**
      * @param mainMenu
      */
     public GuiAbout(GuiScreen mainMenu)
     {
-        this.mainMenu=mainMenu;
-        listWidth =20;
+        this.mainMenu = mainMenu;
+        this.listWidth = 20;
     }
-
+    
     /**
      * Adds the buttons (and other controls) to the screen in question.
      */
-    @SuppressWarnings("unchecked")
     @Override
     public void initGui()
     {
-    	this.buttonList.add(new GuiButton(6, this.width / 2 - 100, this.height - 38, I18n.format("gui.done")));
-        /*  configModButton = new GuiButton(20, 10, this.height - 60, this.listWidth, 20, "Config");
-        disableModButton = new GuiButton(21, 10, this.height - 38, this.listWidth, 20, "Disable");
-        this.buttonList.add(configModButton);
-        this.buttonList.add(disableModButton);
-   */
+        this.buttonList.add(new GuiButton(6, this.width / 2 - 100, this.height - 38, I18n.format("gui.done")));
+        /*
+         * configModButton = new GuiButton(20, 10, this.height - 60,
+         * this.listWidth, 20, "Config");
+         * disableModButton = new GuiButton(21, 10, this.height - 38,
+         * this.listWidth, 20, "Disable");
+         * this.buttonList.add(configModButton);
+         * this.buttonList.add(disableModButton);
+         */
     }
-
+    
     @Override
-    protected void actionPerformed(GuiButton button) {
+    protected void actionPerformed(GuiButton button)
+    {
         if (button.enabled)
         {
             switch (button.id)
             {
                 case 6:
-                	this.mc.displayGuiScreen(this.mainMenu);
+                    this.mc.displayGuiScreen(this.mainMenu);
                     return;
+                default:
+                    break;
             }
         }
         super.actionPerformed(button);
     }
-
+    
     public int drawLine(String line, int offset, int shifty)
     {
         this.fontRendererObj.drawString(line, offset, shifty, 0xd7edea);
         return shifty + 12;
     }
-
+    
     /**
      * Draws the screen and all the components in it.
      */
     @Override
     public void drawScreen(int p_571_1_, int p_571_2_, float p_571_3_)
     {
-    	super.drawBackground(0);
-    	GL11.glEnable(GL11.GL_BLEND);
-    	this.drawCenteredString(this.fontRendererObj, "About", this.width / 2, 16, 0xFFFFFF);
-    	 int offset = this.listWidth  + 20;
-         int shifty = 35;
-         shifty = drawLine("this is a new system for loading addons for Core-API and othe mods", offset, shifty);
-         shifty = drawLine("this was Created by RoboRave", offset, shifty);
-         GL11.glDisable(GL11.GL_BLEND);
-       super.drawScreen(p_571_1_, p_571_2_, p_571_3_);
+        super.drawBackground(0);
+        GL11.glEnable(GL11.GL_BLEND);
+        this.drawCenteredString(this.fontRendererObj, "About", this.width / 2, 16, 0xFFFFFF);
+        int offset = this.listWidth + 20;
+        int shifty = 35;
+        shifty = drawLine("this is a new system for loading addons for Core-API and othe mods", offset, shifty);
+        shifty = drawLine("this was Created by RoboRave", offset, shifty);
+        GL11.glDisable(GL11.GL_BLEND);
+        super.drawScreen(p_571_1_, p_571_2_, p_571_3_);
     }
-
-    Minecraft getMinecraftInstance() {
+    
+    Minecraft getMinecraftInstance()
+    {
         /**
          * Reference to the Minecraft object.
          */
-        return mc;
+        return this.mc;
     }
-
-    FontRenderer getFontRenderer() {
+    
+    FontRenderer getFontRenderer()
+    {
         /**
          * The FontRenderer used by GuiScreen
          */
-        return fontRendererObj;
+        return this.fontRendererObj;
     }
-
+    
 }
--- src/main/java/info/coremodding/api/gui/slot/GuiSlotMod.java
+++ src/main/java/info/coremodding/api/gui/slot/GuiSlotMod.java
@@ -1,4 +1,3 @@
-
 package info.coremodding.api.gui.slot;
 
 import info.coremodding.api.gui.GuiBSConfig;
@@ -12,60 +11,63 @@ import cpw.mods.fml.client.GuiScrollingList;
 
 /**
  * @author RoboRave
- *
+ * 
  */
 public class GuiSlotMod extends GuiScrollingList
 {
-    private GuiBSConfig parent;
-    private List<ModPlugin> mods;
-
+    
+    private final GuiBSConfig     parent;
+    private final List<ModPlugin> mods;
+    
     public GuiSlotMod(GuiBSConfig parent, List<ModPlugin> mods2, int listWidth)
     {
         super(parent.getMinecraftInstance(), listWidth, parent.height, 32, parent.height - 66 + 4, 10, 35);
-        this.parent=parent;
-        this.mods=mods2;
+        this.parent = parent;
+        this.mods = mods2;
     }
-
+    
     @Override
     protected int getSize()
     {
-        return mods.size();
+        return this.mods.size();
     }
-
+    
     @Override
     protected void elementClicked(int var1, boolean var2)
     {
         this.parent.selectModIndex(var1);
     }
-
+    
     @Override
     protected boolean isSelected(int var1)
     {
         return this.parent.modIndexSelected(var1);
     }
-
+    
     @Override
     protected void drawBackground()
     {
         this.parent.drawDefaultBackground();
     }
-
+    
     @Override
     protected int getContentHeight()
     {
         return (this.getSize()) * 35 + 1;
     }
-
+    
     @SuppressWarnings("static-access")
-	@Override
+    @Override
     protected void drawSlot(int listIndex, int var2, int var3, int var4, Tessellator var5)
     {
-        ModPlugin mc= Loader.mods.get(listIndex);
+        ModPlugin mc = Loader.mods.get(listIndex);
         
-            this.parent.getFontRenderer().drawString(this.parent.getFontRenderer().trimStringToWidth(mc.getMeta().name, listWidth - 10), this.left + 3 , var3 + 2, 0xFFFFFF);
-            this.parent.getFontRenderer().drawString(this.parent.getFontRenderer().trimStringToWidth(mc.getMeta().version, listWidth - 10), this.left + 3 , var3 + 12, 0xCCCCCC);
-            //this.parent.getFontRenderer().drawString(this.parent.getFontRenderer().trimStringToWidth(mc.getMetadata() !=null ? mc.getMetadata().getChildModCountString() : "Metadata not found", listWidth - 10), this.left + 3 , var3 + 22, 0xCCCCCC);
+        this.parent.getFontRenderer().drawString(this.parent.getFontRenderer().trimStringToWidth(mc.getMeta().name, this.listWidth - 10), this.left + 3, var3 + 2, 0xFFFFFF);
+        this.parent.getFontRenderer().drawString(this.parent.getFontRenderer().trimStringToWidth(mc.getMeta().version, this.listWidth - 10), this.left + 3, var3 + 12, 0xCCCCCC);
+        // this.parent.getFontRenderer().drawString(this.parent.getFontRenderer().trimStringToWidth(mc.getMetadata()
+        // !=null ? mc.getMetadata().getChildModCountString() :
+        // "Metadata not found", listWidth - 10), this.left + 3 , var3 + 22,
+        // 0xCCCCCC);
         
     }
-
 }
