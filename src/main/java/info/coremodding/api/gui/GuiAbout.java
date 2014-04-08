9371e1cf4e2aa68732d6fafb194c85e1ae5df4ce
RoboRave <zdowding@gmail.com>
2014-04-07 19:07:05 -0600

    updated and rewrote GUIABout

--- src/main/java/info/coremodding/api/gui/GuiAbout.java
+++ src/main/java/info/coremodding/api/gui/GuiAbout.java
@@ -1,23 +1,17 @@
 package info.coremodding.api.gui;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 
 import org.lwjgl.opengl.GL11;
 
-/**
- * @author Roborave
- * 
- */
+
 public class GuiAbout extends GuiScreen
 {
     
     private final GuiScreen mainMenu;
     private final int       listWidth;
-    
     /**
      * @param mainMenu
      */
@@ -25,6 +19,14 @@ public class GuiAbout extends GuiScreen
     {
         this.mainMenu = mainMenu;
         this.listWidth = 20;
+    }
+    /**
+     * Adds the buttons (and other controls) to the screen in question.
+     */
+    @Override
+    public void initGui()
+    {
+        this.buttonList.add(new GuiButton(6, this.width / 2 - 100, this.height - 38, I18n.format("gui.done")));
     }
     
     @Override
@@ -35,7 +37,7 @@ public class GuiAbout extends GuiScreen
             switch (button.id)
             {
                 case 6:
-                    this.mc.displayGuiScreen(this.mainMenu);
+                   MinecraftHelper.displayGuiScreen(mc, this.mainMenu);
                     return;
                 default:
                     break;
@@ -57,39 +59,11 @@ public class GuiAbout extends GuiScreen
     public void drawScreen(int p_571_1_, int p_571_2_, float p_571_3_)
     {
         super.drawBackground(0);
-        GL11.glEnable(GL11.GL_BLEND);
         this.drawCenteredString(this.fontRendererObj, "About", this.width / 2, 16, 0xFFFFFF);
         int offset = this.listWidth + 20;
         int shifty = 35;
         shifty = drawLine("this is a new system for loading addons for Core-API and othe mods", offset, shifty);
         shifty = drawLine("this was Created by RoboRave", offset, shifty);
-        GL11.glDisable(GL11.GL_BLEND);
         super.drawScreen(p_571_1_, p_571_2_, p_571_3_);
     }
-    
-    FontRenderer getFontRenderer()
-    {
-        /**
-         * The FontRenderer used by GuiScreen
-         */
-        return this.fontRendererObj;
-    }
-    
-    Minecraft getMinecraftInstance()
-    {
-        /**
-         * Reference to the Minecraft object.
-         */
-        return this.mc;
-    }
-    
-    /**
-     * Adds the buttons (and other controls) to the screen in question.
-     */
-    @Override
-    public void initGui()
-    {
-        this.buttonList.add(new GuiButton(6, this.width / 2 - 100, this.height - 38, I18n.format("gui.done")));
-    }
-    
 }
