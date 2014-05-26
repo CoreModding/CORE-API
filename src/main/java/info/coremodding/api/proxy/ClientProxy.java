ae1df443d8d05e669af256b013780290b002be27
RoboRave <zdowding@gmail.com>
2014-05-26 10:55:43 -0600

    Revert "Added GradleWrapper"
    This reverts commit 7f51236196a3beefd223e8af64b386d013af8506.

--- src/main/java/info/coremodding/api/proxy/ClientProxy.java
+++ src/main/java/info/coremodding/api/proxy/ClientProxy.java
@@ -7,6 +7,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ThreadDownloadImageData;
 import net.minecraft.client.renderer.texture.ITextureObject;
 import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.relauncher.Side;
 
 /**
  * @author ethan
@@ -20,12 +21,20 @@ public class ClientProxy extends CommonProxy{
 	public void registerTeamCapes() {
 		final String capeURL = "http://www.mccapes.com/GalleryImages6x/0c1865261d2d0247fb6b776bdc5d6730.png"; // Temporary. Can someone make us a proper cape? We should be able to have a HD one I think!
 		
-		String[] teamMembers = {"nxsupert"}; // Please add your minecraft user names here. You will get a cape if you do so!
+		String[] teamMembers = {"nxsupert, ProfessorVennie"}; // Please add your minecraft user names here. You will get a cape if you do so!
 
     	ThreadDownloadImageData capeImage = new ThreadDownloadImageData(capeURL, null, null);
 
     	for (String username : teamMembers) {
     		Minecraft.getMinecraft().renderEngine.loadTexture(new ResourceLocation("cloaks/" + username), (ITextureObject) capeImage);
     	}
+	}
+	
+	/**
+	 * @return If the current instance is server or client side.
+	 */
+	@Override()
+	public Side getSide() {
+		return Side.CLIENT;
 	}
 }
