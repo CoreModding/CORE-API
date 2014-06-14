2718881b02ab24575df04e71e75caa2cf3a00b5a
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-06-14 09:07:55 -0700

    Added MCWorldBoundObject

---
+++ src/main/java/info/coremodding/api/locations/MCWorldBoundObject.java
@@ -0,0 +1,41 @@
+package info.coremodding.api.locations;
+
+import net.minecraft.world.World;
+
+/**
+ * @author Zeus
+ * An object bound to an MC world
+ */
+public class MCWorldBoundObject extends Traveller
+{
+
+    /**
+     * The world that this is bound to
+     */
+    public World world;
+    
+    /**
+     * @param location The location this exists at
+     * @param movement The way that this moves
+     * @param world The world that this is bound to
+     */
+    public MCWorldBoundObject(Loc3 location, Velocity movement, World world)
+    {
+        super(location, movement);
+        this.world = world;
+    }
+
+    /**
+     * @return The world that this is bound to
+     */
+    public World getWorld(){
+        return this.world;
+    }
+    
+    /**
+     * @param world The new world this is bound to
+     */
+    public void setWorld(World world){
+        this.world = world;
+    }
+}
